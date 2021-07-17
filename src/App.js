263d42d3ff2573053e99ed7f0a3181fb8340d938
muiceking <scm1400@gmail.com>
2021-07-17 15:42:21 +0900

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
+import TopMenuComponent from "./componnents/TopMenuComponent";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    //const baseUrl = "http://localhost:8080";
+    
+    return (
+        <div className="App">
+            <div>
+                <TopMenuComponent>
+                </TopMenuComponent>
+            </div>
+        </div>
+    );
 }
+
 
 export default App;
